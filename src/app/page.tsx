b1e0339332961f9c45515e9b583780d5af700cc2
MostafaKh1 <chiiit1754@gmail.com>
2026-02-22 14:06:13 +0200

    check commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import { Suspense } from "react"
-import {ProductGrid} from "@/components/product/product-grid"
-import {FilterProduct} from "@/components/product/filter-product"
-import { FilterSkeleton, GridSkeleton } from "@/components/product/loading"
+import { Suspense } from 'react';
+import { ProductGrid } from '@/components/product/product-grid';
+import { FilterProduct } from '@/components/product/filter-product';
+import { FilterSkeleton, GridSkeleton } from '@/components/product/loading';
 
 export default async function HomePage({
   searchParams,
 }: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const params = await searchParams
+  const params = await searchParams;
 
   return (
     <div className="container mx-auto px-4 md:px-6">
       <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-2">
           <h1 className="text-3xl font-bold tracking-tight">New Arrivals</h1>
-          <p className="text-muted-foreground">Explore our latest collection of premium essentials.</p>
+          <p className="text-muted-foreground">
+            Explore our latest collection of premium essentials....
+          </p>
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-12">
@@ -33,7 +35,5 @@ export default async function HomePage({
         </div>
       </div>
     </div>
-  )
+  );
 }
-
-
